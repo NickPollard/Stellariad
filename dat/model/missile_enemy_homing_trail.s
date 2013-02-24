c3e9a7805bbf43e91c5dfa982f6bb583e687edb6
Nick Pollard <nickedwardpollard@gmail.com>
2013-02-24 19:14:18 +0000

    Add trail asset for missile.

---
+++ dat/model/missile_enemy_homing_trail.s
@@ -0,0 +1,4 @@
+(model 
+		(transform (translation (vector 0.0 0.0 0.0 1.0))
+					(ribbonEmitter (filename "dat/vfx/ribbons/ribbon_missile_homing_jet.s"))
+					(ribbonEmitter (filename "dat/vfx/ribbons/ribbon_missile_homing.s"))))
