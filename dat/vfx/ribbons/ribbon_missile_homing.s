a8ce546bb6e94058cb3d5c2b50df0eaff7993cf6
Nick Pollard <nickedwardpollard@gmail.com>
2013-02-24 19:14:56 +0000

    Missile trail ribbon art changes.

--- dat/vfx/ribbons/ribbon_missile_homing.s
+++ dat/vfx/ribbons/ribbon_missile_homing.s
@@ -3,7 +3,7 @@
 									(property_key 0.8 (vector 1.0 0.9 0.8 0.7 ))
 									(property_key 1.0 (vector 1.0 0.5 0.3 0.0 ))))
 					( diffuse_texture "dat/img/trail.tga" )
-					( radius 0.8 )
+					( radius 0.6 )
 					( lifetime 1.0 )
 					( static_texture "true" )
 )
