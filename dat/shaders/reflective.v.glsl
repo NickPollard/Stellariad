680a6162db3d9c85f48ced1b2b07cb5d402dee29
Nick Pollard <nickedwardpollard@gmail.com>
2013-10-16 16:15:03 +0100

    Reflection shader.

--- dat/shaders/reflective.v.glsl
+++ dat/shaders/reflective.v.glsl
@@ -1,23 +1,20 @@
 //#version 110
+// Reflective Vertex Shader
 
 #ifdef GL_ES
 precision mediump float;
 #endif
 
-// Reflective Vertex Shader
-// Attributes
 attribute vec4 position;
 attribute vec4 normal;
 attribute vec4 uv;
 attribute vec4 color;
 
-// Varying
 varying vec4 frag_position;
 varying vec4 cameraSpace_frag_normal;
 varying vec2 texcoord;
 varying float fog;
 
-// Uniform
 uniform	mat4 projection;
 uniform	mat4 modelview;
 
@@ -26,11 +23,7 @@ void main() {
 	frag_position = modelview * position;
 	cameraSpace_frag_normal = modelview * normal;
 	texcoord = uv.xy;
-	// TODO - need to get a proper world position, not model position
-	// This means we need the model matrix separate from the combined model-view
-	//fog = fog( position, frag_position );
 
-	// We can calculate fog per vertex as we know polys will be small for terrain
 	float fog_far		= 450.0;
 	float fog_near		= 200.0;
 	float fog_distance	= 250.0;			// fog_far - fog_near
@@ -40,9 +33,9 @@ void main() {
 	float distant_fog_far		= 700.0;
 	float distant_fog_distance	= 100.0; // distant_fog_far - distant_fog_near
 
-	float height_factor = clamp( ( fog_height - position.y ) / fog_height, 0.0, 1.0 );
 	float distance = sqrt( frag_position.z * frag_position.z + frag_position.x * frag_position.x );
-	float near_fog = clamp(( distance - fog_near ) / fog_distance, 0.0, fog_max ) * height_factor;
-	float far_fog = clamp(( frag_position.z - distant_fog_near ) / distant_fog_distance, 0.0, 1.0 );
-	fog = max( near_fog, far_fog );
+	float height_factor = clamp( ( fog_height - position.y ) / fog_height, 0.0, 1.0 );
+	float near_fog = min(( distance - fog_near ) / fog_distance, fog_max ) * height_factor;
+	float far_fog = ( distance - distant_fog_near ) / distant_fog_distance;
+	fog = clamp( max( near_fog, far_fog ), 0.0, 1.0 );
 }
