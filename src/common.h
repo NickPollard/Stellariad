d1e733a3cefdf116051fa11b417a3a018faa2560
Nick Pollard <nickedwardpollard@gmail.com>
2018-06-12 17:32:17 -0700

    re-add for each (used on android)

--- src/common.h
+++ src/common.h
@@ -105,10 +105,10 @@
 
 // Higher Order macros
 
-//#define for_each( array, size, func, ... )
-	//for ( int i = 0; i < size; ++i ) {
-		//func( array[i], __VA_ARGS__ );
-	//}
+#define for_each( array, size, func, ... ) \
+	for ( int i = 0; i < size; ++i ) { \
+		func( array[i], __VA_ARGS__ ); \
+	}
 
 
 
